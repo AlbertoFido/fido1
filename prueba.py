f63a2ce8ac05349229aa4bbda63851ce10a98589
Jose Alberto <josepago@pc03-t1.inf>
2015-10-02 21:36:19 +0200

    pruebav2

--- prueba.py
+++ prueba.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env phyton 
 #-*- coding: utf-8 -*-
-numero = int(raw_input ("Introduzca por favor un número: "))
-if numero % 2 == 0:
-	print "par"
-else:
-	print "impar"
+n =int(raw_input ("Introduzca por favor un número: "))
+numero = 0
+while numero == 0 :
+	if n % 2 == 0:
+		print "par"
+		numero += 1
+	else:
+		print "impar"
+		n = int(raw_input ( " otro numero, me gustan mas los pares: "))
+
+
