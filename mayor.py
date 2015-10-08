f071a6df7d59760d8ef056dc2ab909247213d5dc
alberto gomez <albertofido92@gmail.com>
2015-10-08 21:47:46 +0200

    mayor V1

---
+++ mayor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+def mayor(n1, n2):
+	if n1 > n2:
+		 salida = "a es mayor que b"
+		 salida = salida + "a es mayor que b otra vez"
+	else:
+		salida = "b es mayor que a"
+		salida +=" o igual"
+	return salida
+	
+	
+
+
