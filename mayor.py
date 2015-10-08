f3d41aea9432b659aa32a31ff7619b0e0a0a9bc1
alberto gomez <albertofido92@gmail.com>
2015-10-08 21:49:40 +0200

    mayor V2

--- mayor.py
+++ mayor.py
@@ -2,12 +2,10 @@
 #-*- coding: utf-8 -*-
 def mayor(n1, n2):
 	if n1 > n2:
-		 salida = "a es mayor que b"
-		 salida = salida + "a es mayor que b otra vez"
+		 salida = n1
 	else:
-		salida = "b es mayor que a"
-		salida +=" o igual"
-	return salida
+		salida = n2 
+		return salida
 	
 	
 
