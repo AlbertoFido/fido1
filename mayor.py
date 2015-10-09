558b1f8029dcc55b3fae666f3e1be7c5d462039a
alberto gomez <albertofido92@gmail.com>
2015-10-09 19:46:29 +0200

    mayor V4

--- mayor.py
+++ mayor.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-def mayor(n1, n2):
-	if n1 > n2:
-		 salida = True
-	else:
-		salida = False
-		return salida
-	
-	
+#Escribe un script que reciba una lista de numeros
+#de tamaño indeterminado por el teclado y muestre por pantalla el mayor
+
+def mayor(numeros):
+	num = 0
+	for a in numeros:
+		if a > num:
+			num = a	
+	return num
+
+bucle = True
+numeros = []
+while bucle:
+	a= int(raw_input("Introduce un numero: "))
+	numeros.append(a)
+	if a == 0: 
+		bucle = False 
+		
+print " el mayor es: ", mayor(numeros) 
+		
 
 
