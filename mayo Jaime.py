45a0cfed5a1159925192a11348e5d787dae1ed42
alberto <albertofido92@gmail.com>
2015-10-15 21:39:38 +0200

    entretenido comprimido

--- mayo Jaime.py
+++ mayo Jaime.py
@@ -4,12 +4,9 @@
 #de tamaño indeterminado pero no infinito por el teclado y muestre por pantalla el mayor
 #Version Jaime
 mayor = 0
-entretenido = True
-while entretenido: #mientras_que_el_usuario_no_se_aburra
-	num = int(raw_input("introduce un numero positivo(negativo si te aburres): "))#pedire un numero
-	if num < 0:
-		entretenido = False
-	elif num > mayor: #mirar si es mayo que e mayo hasta ahora
+num = int(raw_input("introduce un numero positivo(negativo si te aburres): "))#pedire un numero
+while num >= 0: #mientras_que_el_numero_sea_mayor_que_0
+	if num > mayor: #mirar si es mayor que el mayor hasta ahora
 		mayor = num #si es que si tengo un nuevo mayor
-	#si es que no me quedo con el que tenia
+	num = int(raw_input("introduce un numero positivo(negativo si te aburres): "))#pedire un numero
 print "el mayor es: " + str(mayor) #el_mayor
