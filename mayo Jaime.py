552dfd546467d0b2a4f911578e5407cbf4ff6992
alberto <albertofido92@gmail.com>
2015-10-15 21:29:12 +0200

    entretenido

--- mayo Jaime.py
+++ mayo Jaime.py
@@ -4,12 +4,12 @@
 #de tamaño indeterminado pero no infinito por el teclado y muestre por pantalla el mayor
 #Version Jaime
 mayor = 0
-aburrido = False
-while not aburrido: #mientras_que_el_usuario_no_se_aburra
+entretenido = True
+while entretenido: #mientras_que_el_usuario_no_se_aburra
 	num = int(raw_input("introduce un numero positivo(negativo si te aburres): "))#pedire un numero
 	if num < 0:
-		aburrido = True
-	if num > mayor: #mirar si es mayo que e mayo hasta ahora
+		entretenido = False
+	elif num > mayor: #mirar si es mayo que e mayo hasta ahora
 		mayor = num #si es que si tengo un nuevo mayor
 	#si es que no me quedo con el que tenia
 print "el mayor es: " + str(mayor) #el_mayor
