ceb6d9cbe681c780b7ae4944dac16d5502ac522b
alberto <albertofido92@gmail.com>
2015-10-15 21:50:35 +0200

    entretenido comprimido V2

--- mayo Jaime.py
+++ mayo Jaime.py
@@ -9,4 +9,10 @@ while num >= 0: #mientras_que_el_numero_sea_mayor_que_0
 	if num > mayor: #mirar si es mayor que el mayor hasta ahora
 		mayor = num #si es que si tengo un nuevo mayor
 	num = int(raw_input("introduce un numero positivo(negativo si te aburres): "))#pedire un numero
-print "el mayor es: " + str(mayor) #el_mayor
+if mayor == 0:
+	print " no has introducido ningun numero" 
+else:
+	print "el mayor es: " + str(mayor) #el_mayor
+
+
+#Hacer un nuevo commit que muestre un mensaje cuando el usuario se aburra de primeras 
