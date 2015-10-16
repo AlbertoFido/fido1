089b8c045e68987de4a8485271722e901f63ced7
alberto <albertofido92@gmail.com>
2015-10-16 18:52:31 +0200

    mayor compr v3

--- mayo Jaime.py
+++ mayo Jaime.py
@@ -3,13 +3,13 @@
 #Escribe un script que reciba una lista de numeros
 #de tamaño indeterminado pero no infinito por el teclado y muestre por pantalla el mayor
 #Version Jaime
-mayor = 0
 num = int(raw_input("introduce un numero positivo(negativo si te aburres): "))#pedire un numero
+mayor= num
 while num >= 0: #mientras_que_el_numero_sea_mayor_que_0
 	if num > mayor: #mirar si es mayor que el mayor hasta ahora
 		mayor = num #si es que si tengo un nuevo mayor
 	num = int(raw_input("introduce un numero positivo(negativo si te aburres): "))#pedire un numero
-if mayor == 0:
+if mayor <= 0:
 	print " no has introducido ningun numero" 
 else:
 	print "el mayor es: " + str(mayor) #el_mayor
