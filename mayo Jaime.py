29a14572e500cb004e1bfa66dae93f2427aefacf
alberto <albertofido92@gmail.com>
2015-10-23 20:18:45 +0200

    mayor for

--- mayo Jaime.py
+++ mayo Jaime.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-#Escribe un script que reciba una lista de numeros
-#de tamaño indeterminado pero no infinito por el teclado y muestre por pantalla el mayor
-#Version Jaime
-num = int(raw_input("introduce un numero positivo(negativo si te aburres): "))#pedire un numero
-mayor= num
-while num >= 0: #mientras_que_el_numero_sea_mayor_que_0
-	if num > mayor: #mirar si es mayor que el mayor hasta ahora
-		mayor = num #si es que si tengo un nuevo mayor
-	num = int(raw_input("introduce un numero positivo(negativo si te aburres): "))#pedire un numero
-if mayor <= 0:
+num=[]
+def mayo(num):
+	mayor = 0
+	for ele in num:
+		if ele > mayor:
+			mayor = ele
+	return mayor
+	
+numero = int(raw_input("introduce un numero positivo(negativo si te aburres): "))
+ma= numero
+while numero >= 0: 
+	if numero > ma: 
+		num.append(numero) 
+	numero = int(raw_input("introduce un numero positivo(negativo si te aburres): "))
+if ma <= 0:
 	print " no has introducido ningun numero" 
 else:
-	print "el mayor es: " + str(mayor) #el_mayor
-
-
-#Hacer un nuevo commit que muestre un mensaje cuando el usuario se aburra de primeras 
+	print "el numero mayor es: " +str(mayo(num))
