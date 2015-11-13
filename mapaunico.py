caec22b70225da484f5c11852e8eeb9bdce09291
alberto <albertofido92@gmail.com>
2015-11-13 19:44:36 +0100

    mapa termiando

--- mapaunico.py
+++ mapaunico.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-
-#def checkio(listaprueba):
-#	dic={}
-#	for ele in listaprueba:
-#		if listaprueba.count(ele) > 1:
-#			dict.fromkeys(listaprueba)
-#	datar = []
-#	for ele in listaprueba:
-#		if listaprueba.count(ele) > 1:
-#			datar.append(ele)
-#	return datar
+def checkio(listaprueba):
+	datar = []
+	dic={}
+	for ele in listaprueba:
+		if ele in dic:
+			dic[ele] += 1
+		else:
+			dic[ele]=1
+	for ele in listaprueba:
+		if dic[ele] > 1:
+			datar.append(ele)
+	return datar
 listaprueba=[1, 2, 30, 30, 6, 6, 5]
 print listaprueba
-#print checkio(listaprueba) 
-dic = dict.fromkeys(listaprueba, )
-print dic
-pepe= listaprueba.count(30)
-print pepe
+print checkio(listaprueba) 
+
+ 
 
