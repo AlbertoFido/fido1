768922db636e30433b5a49f6d640b8009c181238
Alberto Gomez <albertofido92@gmail.com>
2015-11-26 21:52:28 +0100

    monitos

---
+++ mono.py
@@ -0,0 +1,13 @@
+def count_words(text, words):
+	
+    return 0
+
+
+
+   
+print count_words("How aresjfhdskfhskd you?", {"how", "are", "you", "hello"})
+#== 3, "Example"
+print count_words("Bananas, give me bananas!!!", {"banana", "bananas"})
+#== 2, "BANANAS!"
+print count_words("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",{"sum", "hamlet", "infinity", "anything"})
+#== 1, "Weird text"
