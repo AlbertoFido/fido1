543bdd55cc9fdadac1b7b525edee07aed06a52fc
Jose Alberto <josepago@pc02-t1.inf>
2016-01-20 18:59:03 +0100

    calculadoraV3

--- clacalculadora.py
+++ clacalculadora.py
@@ -13,24 +13,52 @@ class calculadora:
 		m = self.a * self.b 
 		print m 
 	def dividir(self):
-		d =self.a // self.b 
-		print d 
+		d =self.a // self.b
+		print d
+	def mayor(self):
+		if self.a > self.b:
+			print self.a, "es mayor que", self.b 
+		elif self.a < self.b:
+			print self.b, "es mayor que", self.a
+		else:
+			print self.a, "y", self.b, "son iguales"
+	def oorr(self):
+		o=self.a or self.b
+		print o
+	def aann(self):
+		a=self.a and self.b
+		print a
+
+
 bo = True
+operacion = calculadora (int (raw_input ("introduce un numero:")), int (raw_input ("introducir otro numero:")))
 while bo == True:
-	operacion = calculadora (int (raw_input ("introduce un numero:")), int (raw_input ("introducir Otro:"))) 
-	opera= raw_input("indica operacion o pulsa c para salir (+,-,*,/): ")
+	opera= raw_input("indica operacion(+, -, *, /, M para comparar o B para operar en binario), pulsa 'c' para salir o 'r' para resetear : ")
 	if opera == "+":
 		operacion.sumar()
 	elif opera == "-":
-		operaciom.restar()
+		operacion.restar()
 	elif opera == "*":
 		operacion.multiplicar()
 	elif opera == "/":
 		operacion.dividir()
+	elif opera == "m":
+		operacion.mayor()
+	elif opera == "b":
+		operab=raw_input("Indica operacion (or, and)")
+		if operab == "or":
+			operacion.oorr()
+		elif operab == "and":
+			operacion.aann()
+		else:
+			print "error de operacion"
 	elif opera == "c":
-		print "adios"
+		print "adios ^-^"
 		bo= False
+	elif opera == "r":
+		operacion = calculadora (int (raw_input ("introduce un numero:")), int (raw_input ("introducir otro numero:"))) 
 	else:
 		print "error de operacion"
+
 
 
